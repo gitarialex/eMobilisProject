e65f673ec68feb81de500a00e7e0223bd8f48087
gitarialex <mgitarialex@gmail.com>
2024-05-13 22:35:53 +0300

    first commit

---
+++ app/src/main/java/com/example/yourretail/fragments/shopping/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.yourretail.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.yourretail.R
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+}
